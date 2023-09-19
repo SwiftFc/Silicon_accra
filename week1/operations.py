fc8c4eb771b0b135a90d2027931fcac1c35ee5f9
SwiftFc <aseiduhenrydade@gmail.com>
2023-09-19 20:34:07 +0000

    My first commit

--- week1/operations.py
+++ week1/operations.py
@@ -5,7 +5,15 @@ Num2 = input("Enter number2: ")
 Num1 = int(Num1)
 Num2 = int(Num2)
 
-addition = Num1+Num2
+add = Num1+Num2
+subtract = Num1-Num2
+multiply = Num1*Num2
+divide = Num1/Num2
+modulus = Num1%Num2
 
-print(f" The addition is: {addition}")
+print(f" The addition is: {add}")
+print(f" The substraction is: {subtract}")
+print(f" The multiplication is: {multiply}")
+print(f" The division is: {divide}")
+print(f" The modulus is: {modulus}")
 
