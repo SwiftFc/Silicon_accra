effd6fdbc466168c1dfd0f40e127a06890a1eaf1
SwiftFc <aseiduhenrydade@gmail.com>
2023-09-19 21:01:11 +0000

    Variables

---
+++ week1/variables.py
@@ -0,0 +1,20 @@
+#Today we are learning about
+    #Variable
+    #Aritmetic
+    #Comparison
+
+#Calculate the area of Rectangle
+
+#Take two variables named 'length' and 'width'
+#and assign them with '10' and '5' respectively
+length = 10
+width = 5
+
+speed = 10
+distance = 20
+
+#now area is the product of 'length' and 'width'
+area = length*width
+acceleration = speed * distance
+print("The area is:", area)
+print("The acceleration is:", acceleration)
