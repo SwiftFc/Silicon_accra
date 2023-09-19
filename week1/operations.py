0671c85088b9b41b861013dbf346a3ba835af91a
SwiftFc <aseiduhenrydade@gmail.com>
2023-09-19 20:24:59 +0000

    My first commit

---
+++ week1/operations.py
@@ -0,0 +1,11 @@
+#TAKE INPUT FROM THE USER
+Num1 = input("Enter number1: ")
+Num2 = input("Enter number2: ")
+
+Num1 = int(Num1)
+Num2 = int(Num2)
+
+addition = Num1+Num2
+
+print(f" The addition is: {addition}")
+
