f51a8a04be4e8f26c067d022431a48d9b4129fe3
SwiftFc <aseiduhenrydade@gmail.com>
2023-10-02 14:15:15 +0000

    week3 files added

---
+++ week3/functions.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    result = a + b
+    return(result)
+
+result = add(1, 4)
+print(result)
+
+def add_two(number):
+    result = number + 2
+    return result
+
+num = int(input("Enter Number: "))
+result = add_two(num)
+print(result)
